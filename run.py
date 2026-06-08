50470abd838598a581359d4a5aa0248b9bde5350
CoffeeIsAllYouNeed <vrushalgithub2004@gmail.com>
2026-06-08 23:58:55 +0530

    Updated run.py

--- run.py
+++ run.py
@@ -22,17 +22,26 @@ class DataFetch:
         self.last_read_timestamp = None
 
     def fetch_latest_batch(self) -> pd.DataFrame:
-        """Reads the newest 2-second window written by the hardware ingestion thread."""
         try:
+            # Ensure the file doesn't require special permissions to access.
+            # If so, it will return False.
             if not os.path.exists(self.filepath):
                 return pd.DataFrame()
+            
+            # pyarrow to enable multithreading. 
+            full_df = pd.read_parquet(self.filepath, engine="pyarrow")
 
-            full_df = pd.read_parquet(self.filepath)
+            # Return empty df for type consistency.
+            # Empty files are handled in preprocessing.
             if full_df.empty:
                 return pd.DataFrame()
 
             full_df["timestamp"] = pd.to_datetime(full_df["timestamp"])
-            full_df = full_df.sort_values(by="timestamp").reset_index(drop=True)
+
+            # Quicksort has low time complexity and space complexity.
+            # TIME COMPLEXITY: O(nlogn)
+            # SPACE COMPLEXITY: O(logn)
+            full_df = full_df.sort_values(by="timestamp", kind="quicksort").reset_index(drop=True)
 
             latest_time = full_df["timestamp"].iloc[-1]
 
@@ -65,7 +74,9 @@ class KeyboardController:
 class Run:
 
     def __init__(self, port: str = "COM6", baudrate: int = 115200, 
-                 storage_path: str = "data/signal.parquet", batch_duration_sec: int = 2):
+                 storage_path: str = "data/signal.parquet", 
+                 batch_duration_sec: int = 2):
+        
         self.storage_path = storage_path
         self.batch_duration_sec = batch_duration_sec
         
@@ -91,7 +102,7 @@ class Run:
         try:
             self.ingestion_system.collect_data_to_parquet(
                 output_path=self.storage_path, 
-                max_duration_sec=86400
+                max_duration_sec=300
             )
         except Exception:
             self.is_running = False
@@ -109,6 +120,10 @@ class Run:
         self.ingestion_thread = threading.Thread(target=self._background_ingestion, daemon=True)
         self.ingestion_thread.start()
 
+        # Prevents a race condition by giving the background thread time to start.
+        # Allows the hardware microcontroller 5 seconds to reset and initialize.
+        # Allows 1 second for initial sensor data to be written to disk.
+        # Prevents the training step from crashing on a missing or empty file.
         time.sleep(6) 
 
         try:
