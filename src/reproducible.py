48cdcb2f5267bd0e9ce78967ede2b975da7db711
CoffeeIsAllYouNeed <vrushalgithub2004@gmail.com>
2026-06-30 00:28:17 +0530

    add: dvc & separate error blocks for environment setting

--- src/reproducible.py
+++ src/reproducible.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class Reproducible:
 
-    def _set_env_seed(self, seed: int) -> None:
+    def _set_os_environment_seed(self, seed: int) -> None:
         os.environ["PYTHONHASHSEED"] = str(seed)
 
     def _set_random_seed(self, seed: int) -> None:
@@ -13,12 +13,19 @@ class Reproducible:
 
     def _set_numpy_seed(self, seed: int) -> None:
         np.random.seed(seed)
-
-    # To produce reproducible results:
+    
+    # Why seed is set to 42? Thanks to "The Hitchhiker's Guide to the Galaxy".
+    # Change as per your requirement.
     def set_seed(self, seed: int = 42) -> None:
         try:
-            self._set_env_seed(seed)
+            self._set_os_environment_seed(seed)
+        except Exception as e:
+            raise RuntimeError(f"Seed Allotment Failure [OS Environment]: {e}")
+        try:
             self._set_random_seed(seed)
+        except Exception as e:
+            raise RuntimeError(f"Seed Allotment Failure [Random]: {e}")
+        try:
             self._set_numpy_seed(seed)
         except Exception as e:
-            raise RuntimeError(f"Failed to set reproducible seeds: {e}")
+            raise RuntimeError(f"Seed Allotment Failure [NumPy]: {e}")
